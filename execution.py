8f92fbd7d0b80386f91852e65ec35965d82d8011
JeevithaChandru <32543385+JeevithaChandru@users.noreply.github.com>
2023-02-27 18:18:38 +0000

    Add files via upload

---
+++ execution.py
@@ -0,0 +1,62 @@
+'''Python program which reads multiple csv files and merges into single output file
+Python version: 3.10
+Author: Jeevitha Chandrappa'''
+
+import csv
+import os
+import sys
+
+#column which identifies each row uniquely
+unique_identifier = 'ID'
+
+rows_dict = {}
+
+#function which takes input files and path for the output file as params
+def merge_csv(file_names, output_path):
+    for file in file_names:
+        #check for similar file patters, could use regex as well
+        if file.startswith("small_file") and file.endswith(".csv"):
+            with open(file, "r") as f:
+                csv_reader = csv.DictReader(f)
+                for row in csv_reader:
+                    id = row[unique_identifier]
+                    #reading each row from csv files as dict
+                    if id not in rows_dict:
+                        rows_dict[id] = {}
+                    for header, value in row.items():
+                        if header != unique_identifier:
+                            rows_dict[id][header] = value
+
+                #getting the unique headers list
+                headers = [unique_identifier]
+                for id in rows_dict:
+                    row = rows_dict[id]
+                    for header in row.keys():
+                        if header not in headers:
+                            headers.append(header)
+
+    #write to output from the rows_dict
+    with open(output_path + "/output_file.csv", "w", newline="") as f:
+        csv_writer = csv.DictWriter(f, fieldnames=headers)
+        csv_writer.writeheader()
+        #writing back to the csv from dict
+        for id in rows_dict:
+            row = rows_dict[id]
+            row[unique_identifier] = id
+            csv_writer.writerow(row)
+
+if __name__ == '__main__':
+
+    # passing filenames as system arguments
+    # file_names = sys.argv[1:]
+    # if not file_names:
+    #     print("Please provide at least one file name as a command line argument")
+    #     sys.exit(1)
+    # for file_name in file_names:
+    #     if not os.path.isfile(file_name):
+    #         print(f"{file_name} is not a file")
+    #         sys.exit(1)
+
+    file_names = os.listdir(".")
+    output_path = os.getcwd()
+    merge_csv(file_names, output_path)
